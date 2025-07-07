faf25f0f4b2899ef83ddc83dcd5a0117e124684b
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2025-07-07 16:54:53 -0300

    Create IIFEs.js

---
+++ exercicios/IIFEs.js
@@ -0,0 +1,4 @@
+// Funções Imediatamente Invocadas (IIFEs):
+// ()(), ou mais precisamente (function() { ... })();, é uma forma comum de criar e executar imediatamente uma função anônima.
+
+(()=>console.log("hello word"))()
